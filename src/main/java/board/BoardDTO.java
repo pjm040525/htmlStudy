1d9c233519e3066f2238bcbcc1730aa5d4728e94
jeongmin_park <pjm040525@gmail.com>
2026-03-25 21:20:11 +0900

    getter,setter, 생성자 생성

--- src/main/java/board/BoardDTO.java
+++ src/main/java/board/BoardDTO.java
@@ -6,5 +6,43 @@ public class BoardDTO {
     private String title;
     private String content;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public BoardDTO(int id, String name, String title, String content) {
+        this.id = id;
+        this.name = name;
+        this.title = title;
+        this.content = content;
+    }
 
 }
