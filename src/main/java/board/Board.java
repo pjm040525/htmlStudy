5ebe6c0e87d75f0c36e1049db1e3b9d8345e6237
jeongmin_park <pjm040525@gmail.com>
2026-03-26 21:35:25 +0900

    데이터 보관소 및 글 등록 생성

---
+++ src/main/java/board/Board.java
@@ -0,0 +1,9 @@
+package board;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+    public static List<BoardDTO> boardList = new ArrayList<>();
+    public static int sequence = 1;
+}
