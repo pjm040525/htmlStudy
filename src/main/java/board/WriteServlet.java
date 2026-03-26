41df02dae57b3fa41462f873b561b24b7c5a03d3
jeongmin_park <pjm040525@gmail.com>
2026-03-26 21:43:51 +0900

    서블릿 기초 생성

---
+++ src/main/java/board/WriteServlet.java
@@ -0,0 +1,8 @@
+package board;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "WriteServlet", value = "/write")
+public class WriteServlet extends HttpServlet {
+}
