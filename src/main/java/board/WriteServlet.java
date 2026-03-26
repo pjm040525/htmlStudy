5f15d9f230a8052a3a10eb88892ba3b19358303c
jeongmin_park <pjm040525@gmail.com>
2026-03-26 21:48:28 +0900

    jsp에서 값 가져오기

--- src/main/java/board/WriteServlet.java
+++ src/main/java/board/WriteServlet.java
@@ -2,7 +2,16 @@ package board;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "WriteServlet", value = "/write")
 public class WriteServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response){
+
+        String name = request.getParameter("name");
+        String title = request.getParameter("title");
+        String content = request.getParameter("content");
+    }
 }
