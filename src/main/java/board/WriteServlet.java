5b6325b41dd9be4bac6019fe503e6b091f81921a
jeongmin_park <pjm040525@gmail.com>
2026-03-26 21:55:40 +0900

    예외처리추가 및 작성시 redirect로 이동

--- src/main/java/board/WriteServlet.java
+++ src/main/java/board/WriteServlet.java
@@ -4,12 +4,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sound.midi.Sequence;
+import java.io.IOException;
 
 @WebServlet(name = "WriteServlet", value = "/write")
 public class WriteServlet extends HttpServlet {
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response){
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         String name = request.getParameter("name");
         String title = request.getParameter("title");
@@ -17,6 +17,8 @@ public class WriteServlet extends HttpServlet {
 
         BoardDTO nBoard = new BoardDTO(Board.sequence++,name,title,content);
         Board.boardList.add(nBoard);
+
+        response.sendRedirect("wirte.jsp");
 
     }
 }
